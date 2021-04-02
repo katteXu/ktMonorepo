1835bf0d0d7605b3aed4e97bd6a33787f0c38aa4
xu <417560450@qq.com>
2021-04-02 12:02:52 +0800

    配置icon

--- components/icon.js
+++ components/icon.js
@@ -1,38 +1,38 @@
 const CONFIG = require('../config');
 
-const Logo = `${CONFIG.public_path}icon/logo.svg`;
-const Transport = `${CONFIG.public_path}icon/transport-icon.svg`;
-const Logistics = `${CONFIG.public_path}icon/logistics-icon.svg`;
-const Stock = `${CONFIG.public_path}icon/stock-icon.svg`;
-const Finance = `${CONFIG.public_path}icon/finance-icon.svg`;
-const User = `${CONFIG.public_path}icon/user-icon.svg`;
-const People = `${CONFIG.public_path}icon/people.svg`;
-const Phone = `${CONFIG.public_path}icon/phone.svg`;
-const LinkIcon = `${CONFIG.public_path}fx.ico`;
+const Logo = `${CONFIG.public_path}/icon/logo.svg`;
+const Transport = `${CONFIG.public_path}/icon/transport-icon.svg`;
+const Logistics = `${CONFIG.public_path}/icon/logistics-icon.svg`;
+const Stock = `${CONFIG.public_path}/icon/stock-icon.svg`;
+const Finance = `${CONFIG.public_path}/icon/finance-icon.svg`;
+const User = `${CONFIG.public_path}/icon/user-icon.svg`;
+const People = `${CONFIG.public_path}/icon/people.svg`;
+const Phone = `${CONFIG.public_path}/icon/phone.svg`;
+const LinkIcon = `${CONFIG.public_path}/fx.ico`;
 // 概览
-const Wallet = `${CONFIG.public_path}icon/wallet.svg`;
-const Day = `${CONFIG.public_path}icon/day.svg`;
-const Month = `${CONFIG.public_path}icon/month.svg`;
-const Cumulative = `${CONFIG.public_path}icon/cumulative.svg`;
-const Blue = `${CONFIG.public_path}icon/icon-blue.svg`;
-const Red = `${CONFIG.public_path}icon/icon-red.svg`;
-const Yellow = `${CONFIG.public_path}icon/icon-yellow.svg`;
-const Green = `${CONFIG.public_path}icon/icon-green.svg`;
+const Wallet = `${CONFIG.public_path}/icon/wallet.svg`;
+const Day = `${CONFIG.public_path}/icon/day.svg`;
+const Month = `${CONFIG.public_path}/icon/month.svg`;
+const Cumulative = `${CONFIG.public_path}/icon/cumulative.svg`;
+const Blue = `${CONFIG.public_path}/icon/icon-blue.svg`;
+const Red = `${CONFIG.public_path}/icon/icon-red.svg`;
+const Yellow = `${CONFIG.public_path}/icon/icon-yellow.svg`;
+const Green = `${CONFIG.public_path}/icon/icon-green.svg`;
 
 // 运单管理
-const TransportIcon = `${CONFIG.public_path}icon/transportIcon.svg`;
-const RouteIcon = `${CONFIG.public_path}icon/routeIcon.svg`;
-const SpeakerIcon = `${CONFIG.public_path}icon/speakerIcon.svg`;
-const CarIcon = `${CONFIG.public_path}icon/carIcon.svg`;
+const TransportIcon = `${CONFIG.public_path}/icon/transportIcon.svg`;
+const RouteIcon = `${CONFIG.public_path}/icon/routeIcon.svg`;
+const SpeakerIcon = `${CONFIG.public_path}/icon/speakerIcon.svg`;
+const CarIcon = `${CONFIG.public_path}/icon/carIcon.svg`;
 
 // 智慧工厂
-const ArrowIcon = `${CONFIG.public_path}icon/blue-arrow.svg`;
-const TotalPic = `${CONFIG.public_path}icon/total.svg`;
-const CleanCoalPic = `${CONFIG.public_path}icon/cleanCoal.svg`;
+const ArrowIcon = `${CONFIG.public_path}/icon/blue-arrow.svg`;
+const TotalPic = `${CONFIG.public_path}/icon/total.svg`;
+const CleanCoalPic = `${CONFIG.public_path}/icon/cleanCoal.svg`;
 
 // 个人中心
-const VerifyNone = `${CONFIG.public_path}icon/verify_none.svg`;
-const VerifyIcon = `${CONFIG.public_path}icon/verify.svg`;
+const VerifyNone = `${CONFIG.public_path}/icon/verify_none.svg`;
+const VerifyIcon = `${CONFIG.public_path}/icon/verify.svg`;
 
 export {
   LinkIcon,
