eebf194d7b910fb290e8eaf7178056c1ce06911e
xu <417560450@qq.com>
2021-04-06 10:30:36 +0800

    移除冗余代码

--- bin/build.sh
+++ bin/build.sh
@@ -2,9 +2,9 @@ echo --------------------------开始构建\<$1\>--------------------------
 
 # 删除冗余资源
 rm -rf node_modules
-# rm -rf out/_next
-# rm -rf out/icon
-# rm -rf out/image
+rm -rf out/_next
+rm -rf out/icon
+rm -rf out/image
 
 # 安装服务端依赖
 yarn install --production
