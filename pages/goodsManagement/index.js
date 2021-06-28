605947092c31eebafc2565c2183bb3e4cdcac737
塞内加尔 <yanni_6666@163.com>
2021-06-28 10:59:31 +0800

    货品管理列表分页组件修改

--- pages/goodsManagement/index.js
+++ pages/goodsManagement/index.js
@@ -286,9 +286,9 @@ const GoodsManagement = props => {
 
   // 分页
   const onChangePage = useCallback(
-    page => {
-      setQuery({ ...query, page });
-      getRemoteData({ ...query, page });
+    (page, pageSize) => {
+      setQuery({ ...query, page, pageSize });
+      getRemoteData({ ...query, page, pageSize });
     },
     [dataList]
   );
@@ -510,7 +510,6 @@ const GoodsManagement = props => {
             columns={columns}
             pagination={{
               onChange: onChangePage,
-              onShowSizeChange: onChangePageSize,
               showSizeChanger: true,
               pageSize: query.pageSize,
               current: query.page,
