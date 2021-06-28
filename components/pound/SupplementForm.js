c3c15fddfd6c7ab6d0068488bbaeea74a865bfa7
塞内加尔 <yanni_6666@163.com>
2021-06-28 15:50:36 +0800

    样式修改

--- components/pound/SupplementForm.js
+++ components/pound/SupplementForm.js
@@ -441,7 +441,7 @@ const EditableTable = props => {
               placement="topRight"
               icon={<QuestionCircleFilled />}
               onConfirm={() => handleRemove(record.key)}>
-              <Button type="link" className={deleteBtn.delete} style={{ padding: 0, border: 'none', margin: 0 }}>
+              <Button type="link" danger style={{ padding: 0, border: 'none', margin: 0 }}>
                 删除
               </Button>
             </Popconfirm>
