c1a7c8aa37ffc8e25845a590a5be92969d6a3ce4
zhangleifeng <zhangleifeng@kcsj.onaliyun.com>
2021-11-29 16:02:39 +0800

    开票页面含税总额显示调整

--- components/Finance/main/UnBilling.js
+++ components/Finance/main/UnBilling.js
@@ -308,7 +308,7 @@ const UnBilling = props => {
         <span className="total-num">{Format.weight(total.weight)}</span>吨
         <span style={{ marginLeft: 32 }}>运费总额</span>
         <span className="total-num">{Format.price(total.price)}</span>元<span style={{ marginLeft: 32 }}>含税总额</span>
-        <span className="total-num">{Format.price(total.invoice_price)}</span>元
+        <span className="total-num">{Format.price(total.taxAmount)}</span>元
       </Msg>
 
       <Table
