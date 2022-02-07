afe3ec8631cf81c31f49061cf08d18434d6e4233
bitzlf <bitzlf@163.com>
2022-02-07 11:46:16 +0800

    和顺模式判断错误修复

--- store/WhiteList.js
+++ store/WhiteList.js
@@ -18,10 +18,10 @@ const useWhiteList = () => {
     if (res.status === 0) {
       const { userId } = localStorage;
 
-      const HE_SHUN_ID = res.result.find(
-        item => item.key === 'HE_SHUN_GOODSOWNER_ID_WHITE' || item.key === 'LU_QIAO_GOODSOWNER_ID_WHITE'
-      ).url;
-      if (HE_SHUN_ID && HE_SHUN_ID.includes(userId)) {
+      const HE_SHUN_ID = res.result.find(item => item.key === 'HE_SHUN_GOODSOWNER_ID_WHITE').url;
+      const LU_QIAO_ID = res.result.find(item => item.key === 'LU_QIAO_GOODSOWNER_ID_WHITE').url;
+
+      if ((HE_SHUN_ID && HE_SHUN_ID.includes(userId)) || (LU_QIAO_ID && LU_QIAO_ID.includes(userId))) {
         setWhiteList({
           ...whiteList,
           heShun: true,
